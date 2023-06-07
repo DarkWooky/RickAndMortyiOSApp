e5b27611f56b18c923e8cb0181dec29b60c6f000
Egor Mikhalevich <y.mikhalevich@qittiq.com>
2023-06-07 03:29:11 +0300

    Added docstrings and API related files

---
+++ RickAndMorty/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorty
+//
+//  Created by Egor Mikhalevich on 7.06.23.
+//
+
+import Foundation
+
+/// Object that represents a single API call
+final class RMRequest {
+    
+}
